81cbe73f13d3f6ade43519bf1cbc285530160d42
Florentin <florentin.bouchez@gmail.com>
2024-10-09 21:44:58 +0200

    style: Update organisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_devcamp_ui/src/application.dart';
+import 'package:flutter_devcamp_ui/src/feature/application/application.dart';
 
 void main() {
   runApp(const Application());
